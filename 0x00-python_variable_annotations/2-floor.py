354cbc556e6a38a2a6df564e3d12e7cd74ef14bb
godwinn22 <godwinohazulike@gmail.com>
2024-08-03 18:05:41 +0100

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This is a module that creates a type-annotated function floor which
+takes a float n as argument and returns the floor of the float.
+
+Keyword arguments:
+n -- float
+Return: returns the floor of the float.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """This method returns the floor of the float.
+    Args:
+    n (float): the first float
+
+    Returns:
+    int: returns the floor of the float."""
+    return math.floor(n)
